61b1226292e14a14b620aaa25fc94e21381dab4e
Pat Eason <patrick.eason@msj.org>
2018-07-09 09:49:01 -0400

    > ShirtRepository scoped to Ember.Api.Startup

--- src/Ember.Api/Startup.cs
+++ src/Ember.Api/Startup.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Ember.Domain.Interfaces;
+using Ember.Infrastructure.Context;
+using Ember.Infrastructure.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -15,16 +20,27 @@ namespace Api
 {
   public class Startup
   {
-    public Startup(IConfiguration configuration)
-    {
-      Configuration = configuration;
-    }
-
     public IConfiguration Configuration { get; }
+    
+    public Startup(IHostingEnvironment env)
+    {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+      builder.AddEnvironmentVariables();
+      Configuration = builder.Build();
+    }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<EmberContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+      
+      services.AddScoped<IShirtRepository, ShirtRepository>();
+      
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
     }
 
---
+++ src/Ember.Infrastructure/Repository/ShirtRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ember.Domain.Interfaces;
+using Ember.Domain.Models;
+using Ember.Infrastructure.Context;
+
+namespace Ember.Infrastructure.Repository
+{
+  public class ShirtRepository : Repository<Shirt>, IShirtRepository
+  {
+    public ShirtRepository(EmberContext context) : base(context)
+    {
+    }
+  }
+}
